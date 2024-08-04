1a963a3b200264c1821c39ebf68ff495efd0b81a
Brendan Jurd <direvus@gmail.com>
2024-08-04 18:52:15 +1000

    Solve Day 7 Part 2.

--- advent07.py
+++ advent07.py
@@ -2,11 +2,34 @@
 import sys
 
 
-TRANS = str.maketrans('23456789TJQKA', 'abcdefghijklm')
+STD = str.maketrans('23456789TJQKA', 'abcdefghijklm')
+WILD = str.maketrans('J23456789TQKA', 'abcdefghijklm')
 
 
-def get_hand_type(hand: str) -> int:
+def get_card_counts(hand: str) -> list:
+    """Return a list of counts for each card.
+
+    Returns a list of tuples containing the count and card for each
+    card present in the hand. For example, a hand of KKTT2 would give
+    the result:
+
+        [(2, 'K'), (2, 'T'), (1, '2')]
+
+    Results are ordered from the highest to the lowest card count. If
+    multiple cards share the same count then they are returned in no
+    specific order.
+    """
+    cards = set(hand)
+    counts = [(hand.count(x), x) for x in cards]
+    counts.sort(key=lambda x: x[0], reverse=True)
+    return counts
+
+
+def get_hand_type(hand: str, wilds: bool = False) -> int:
     """Return the type of hand, given a 5-character hand string.
+
+    If `wilds` is true then we use the alternative rules where the J
+    card is allowed to act as a wildcard.
 
     The returned value is an integer ranging from 0-6 as follows:
 
@@ -18,19 +41,27 @@ def get_hand_type(hand: str) -> int:
     - 1 = One pair
     - 0 = Nothing
     """
-    s = set(hand)
-    match len(s):
+    counts = get_card_counts(hand)
+    if wilds and 'J' in hand:
+        # Replace all wilds with whichever card has the highest count.
+        #
+        # If two cards tie for highest count, it doesn't matter which
+        # one we choose so don't worry about it.
+        sub = [card for count, card in counts if card != 'J']
+        if sub:
+            hand = hand.replace('J', sub[0])
+        counts = get_card_counts(hand)
+
+    match len(counts):
         case 1:
             # Five of a kind
             return 6
         case 2:
             # Either a full house or a four-of-a-kind
-            count = hand.count(hand[0])
-            return 5 if count in {1, 4} else 4
+            return 5 if counts[0][0] == 4 else 4
         case 3:
             # Either a three-of-a-kind or a two pair
-            counts = {hand.count(x) for x in s}
-            return 3 if 3 in counts else 2
+            return 3 if counts[0][0] == 3 else 2
         case 4:
             # One pair
             return 1
@@ -39,13 +70,18 @@ def get_hand_type(hand: str) -> int:
             return 0
 
 
-def get_hand_key(hand: str) -> str:
+def get_hand_key(hand: str, wilds: bool = False) -> str:
     """Return a sort key for a hand.
+
+    If `wilds` is true then we use the alternative rules where the J
+    card is considered the weakest for tie breaking purposes.
 
     This key can be passed to a sort method to yield a correct sort
     order from weakest to strongest, across any collection of hands.
     """
-    return str(get_hand_type(hand)) + hand.translate(TRANS)
+    table = WILD if wilds else STD
+    typ = get_hand_type(hand, wilds)
+    return str(typ) + hand.translate(table)
 
 
 if __name__ == '__main__':
@@ -61,3 +97,11 @@ if __name__ == '__main__':
         total1 += i * bid
         i += 1
     print(total1)
+
+    hands.sort(key=lambda x: get_hand_key(x[0], True))
+    total2 = 0
+    i = 1
+    for hand, bid in hands:
+        total2 += i * bid
+        i += 1
+    print(total2)
