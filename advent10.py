385d74f960c0dafbea8a70369fd9c423fc578548
Brendan Jurd <direvus@gmail.com>
2024-08-05 16:58:24 +1000

    Solved Day 10 Part 2.

--- advent10.py
+++ advent10.py
@@ -10,12 +10,15 @@ class Direction(Enum):
     WEST = auto()
 
 
+# These exit directions are set up so that for corner nodes, the first exit is
+# in the clockwise direction of travel, and the second exit is in the
+# anti-clockwise direction.
 PIPES = {
         '|': (Direction.NORTH, Direction.SOUTH),
         '-': (Direction.WEST, Direction.EAST),
         'L': (Direction.NORTH, Direction.EAST),
-        'J': (Direction.NORTH, Direction.WEST),
-        '7': (Direction.WEST, Direction.SOUTH),
+        'J': (Direction.WEST, Direction.NORTH),
+        '7': (Direction.SOUTH, Direction.WEST),
         'F': (Direction.EAST, Direction.SOUTH),
         }
 
@@ -97,3 +100,40 @@ if __name__ == '__main__':
         b = traverse(grid, b)
         steps += 1
     print(f"{steps} steps to reach the farthest tile")
+
+    # Part 2 - tiles enclosed by the loop.
+    #
+    # First we'll traverse the whole loop and collect all the points along it.
+    v = (start[0], start[1], PIPES[home][0])
+    nodes = [start]
+    while v[:2] != start or len(nodes) == 1:
+        v = traverse(grid, v)
+        node = v[:2]
+        nodes.append(node)
+
+    # Now we will scan all tiles, row by row. On each row we start "outside"
+    # the loop. Each time we cross over the pipes we flip between "inside" and
+    # "outside". Running along the pipes doesn't flip the state, but we have to
+    # be careful about these two special cases:
+    #
+    #     |            |
+    #     L---7    F---J
+    #         |    |
+    count = 0
+    cross_corner = None
+    for i in range(height):
+        inside = False
+        for j in range(width):
+            node = (i, j)
+            if node in nodes:
+                glyph = grid[i][j]
+                if glyph in {'|', cross_corner}:
+                    inside = not inside
+                    cross_corner = None
+                elif glyph == 'L':
+                    cross_corner = '7'
+                elif glyph == 'F':
+                    cross_corner = 'J'
+            elif inside:
+                count += 1
+    print(f"{count} internal tiles")
