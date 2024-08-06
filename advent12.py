62efe780fbf72f21b5769a498865406f969ff2ad
Brendan Jurd <direvus@gmail.com>
2024-08-06 22:38:09 +1000

    Optimise in Day 12.
    This change to the way we detect a valid attempt is better than the
    previous by up to 8x, which is great but the solution is still way too
    slow to cope with Part 2.
    
    I'll need to come up with something radically different.

--- advent12.py
+++ advent12.py
@@ -14,15 +14,14 @@ def count_ways(row: str, runs: tuple[int]) -> int:
     if unknown == missing:
         return 1
 
+    rex = re.compile(r'\.*' + r'\.+'.join(['#' * x for x in runs]) + r'\.*')
     indexes = [i for i, x in enumerate(row) if x == '?']
     ways = 0
     for comb in combinations(indexes, missing):
         attempt = ''.join([
                 x if i not in indexes else '#' if i in comb else '.'
                 for i, x in enumerate(row)])
-        parts = RUN_RE.findall(attempt)
-        lengths = tuple((len(x) for x in parts))
-        if lengths == runs:
+        if rex.fullmatch(attempt):
             ways += 1
     return ways
 
@@ -32,9 +31,20 @@ if __name__ == '__main__':
     for line in sys.stdin:
         field, runs = line.strip().split()
         runs = tuple([int(x) for x in runs.split(',')])
-        rows.append((field, runs))
+        rows.append([field, runs])
 
+    # Part 1
     total = 0
     for row, runs in rows:
         total += count_ways(row, runs)
     print(total)
+
+    # Part 2 - expand all inputs rows by 5x
+    for i, [row, runs] in enumerate(rows):
+        rows[i][0] = '?'.join([row] * 5)
+        rows[i][1] = runs * 5
+
+    total = 0
+    #for row, runs in rows:
+    #    total += count_ways(row, runs)
+    #print(total)
