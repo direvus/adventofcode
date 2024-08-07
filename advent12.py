d195f4430e5d70129eac67081f7c156957047e4b
Brendan Jurd <direvus@gmail.com>
2024-08-07 21:02:32 +1000

    Optimise Day 12 a bit more.
    By eliminating some logically impossible scenarios, we can speed up some
    of these by a whole lot ... but that still leaves many of them
    unsolvable in a practical timeframe.
    
    I really need a completely different way of looking at this problem.

--- advent12.py
+++ advent12.py
@@ -7,15 +7,51 @@ from itertools import combinations
 RUN_RE = re.compile(r'[#?]+')
 
 
+def replace(source: str, position: int, replacement: str):
+    return source[:position] + replacement + source[position + 1:]
+
+
+def simplify(row: str, runs: tuple[int]) -> str:
+    """Simplify a row by eliminating impossible options.
+
+    We iterate through each of the unknown positions (marked with ?) and try
+    substituting in a dot, and a hash. If only one of those substitutions still
+    fits the run pattern, we can resolve the unknown marker and replace it
+    permanently with a dot or a hash.
+    """
+    indexes = tuple((i for i, x in enumerate(row) if x == '?'))
+    run_pattern = r'[.?]+'.join([f'[#?]{{{x}}}' for x in runs])
+    rex = re.compile(r'[.?]*' + run_pattern + r'[.?]*')
+
+    for i in indexes:
+        with_hash = replace(row, i, '#')
+        with_dot = replace(row, i, '.')
+        hash_ok = rex.fullmatch(with_hash)
+        dot_ok = rex.fullmatch(with_dot)
+
+        if dot_ok and not hash_ok:
+            row = with_dot
+        if hash_ok and not dot_ok:
+            row = with_hash
+    return row
+
+
 def count_ways(row: str, runs: tuple[int]) -> int:
     unknown = row.count('?')
     total = sum(runs)
     missing = total - row.count('#')
-    if unknown == missing:
+    if unknown == missing or missing == 0:
+        return 1
+
+    print(f"{row} {runs}")
+    row = simplify(row, runs)
+    indexes = [i for i, x in enumerate(row) if x == '?']
+    unknown = len(indexes)
+    missing = total - row.count('#')
+    if unknown == missing or missing == 0:
         return 1
 
     rex = re.compile(r'\.*' + r'\.+'.join(['#' * x for x in runs]) + r'\.*')
-    indexes = [i for i, x in enumerate(row) if x == '?']
     ways = 0
     for comb in combinations(indexes, missing):
         attempt = ''.join([
@@ -23,6 +59,7 @@ def count_ways(row: str, runs: tuple[int]) -> int:
                 for i, x in enumerate(row)])
         if rex.fullmatch(attempt):
             ways += 1
+    print(f"   {ways} ways")
     return ways
 
 
@@ -45,6 +82,6 @@ if __name__ == '__main__':
         rows[i][1] = runs * 5
 
     total = 0
-    #for row, runs in rows:
-    #    total += count_ways(row, runs)
-    #print(total)
+    for row, runs in rows:
+        total += count_ways(row, runs)
+    print(total)
