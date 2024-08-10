e39865f6132d4a2ae7b65487dcbcadd30910fd2c
Brendan Jurd <direvus@gmail.com>
2024-08-10 16:01:03 +1000

    Further optimise Day 12 Part 2.
    This attempt only calculates the number of solutions for each unique
    grouping of contiguous copies of the row joined by a '#', and from there
    it's just a maths formula to get the answer.
    
    This solves the test case in < 0.2s which is a huge improvement, but it
    is still unable to solve the full input in a reasonable time.

--- advent12.py
+++ advent12.py
@@ -1,108 +1,55 @@
 #!/usr/bin/env python
 import re
 import sys
-import time
-from contextlib import contextmanager
-
-
-RUN_RE = re.compile(r'[#?]+')
-REGEX_TIMINGS = []
-FUNC_TIMINGS = []
-
-
-@contextmanager
-def timing(accumulator=None):
-    try:
-        start = time.perf_counter_ns()
-        yield start
-    finally:
-        duration = time.perf_counter_ns() - start
-        if accumulator is not None:
-            accumulator.append(duration)
 
 
 def replace(source: str, position: int, replacement: str):
     return source[:position] + replacement + source[position + 1:]
 
 
-def check_row(row: str, runs: tuple[int]) -> bool:
-    """Check whether a row matches its run series."""
-    runs = list(runs)
-    in_run = False
-    current_run = None
-    run_size = 0
-    for ch in row:
-        if ch == '#':
-            if not in_run:
-                in_run = True
-                current_run = runs.pop(0)
-                run_size = 1
-            else:
-                run_size += 1
-                if run_size > current_run:
-                    return False
-        elif in_run:
-            if run_size != current_run:
-                return False
-            in_run = False
-    if in_run and run_size != current_run:
-        return False
-
-    return True
+def build_regex(runs: tuple[int]) -> re.Pattern:
+    run_pattern = r'[.?]+'.join([f'[#?]{{{x}}}' for x in runs])
+    return re.compile(r'[.?]*' + run_pattern + r'[.?]*')
 
 
-def check_pattern(pattern: str, row: str) -> bool:
-    """Check whether a row matches its original pattern."""
-    assert len(pattern) == len(row)
-    for i, ch in enumerate(pattern):
-        if ch != '?' and ch != row[i]:
-            return False
-    return True
+def simplify(row: str, regex: re.Pattern) -> str:
+    indexes = [i for i, x in enumerate(row) if x == '?']
+    for i in indexes:
+        with_hash = replace(row, i, '#')
+        with_dot = replace(row, i, '.')
+        hash_ok = regex.fullmatch(with_hash)
+        dot_ok = regex.fullmatch(with_dot)
+
+        if dot_ok and not hash_ok:
+            row = with_dot
+        if hash_ok and not dot_ok:
+            row = with_hash
+    return row
 
 
-def count_ways(
+def get_ways(
         row: str,
         runs: tuple[int],
         regex: re.Pattern = None,
-        firstpass: bool = True) -> int:
+        ) -> list[str]:
+    if not regex:
+        regex = build_regex(runs)
     indexes = [i for i, x in enumerate(row) if x == '?']
     unknown = len(indexes)
     total = sum(runs)
     missing = total - row.count('#')
-    if unknown == missing or missing == 0:
-        return 1
-
-    if not regex:
-        run_pattern = r'[.?]+'.join([f'[#?]{{{x}}}' for x in runs])
-        regex = re.compile(r'[.?]*' + run_pattern + r'[.?]*')
-
-    # First pass: replace unknowns that have only one valid solution
-    if firstpass:
-        for i in indexes:
-            with_hash = replace(row, i, '#')
-            with_dot = replace(row, i, '.')
-            hash_ok = regex.fullmatch(with_hash)
-            dot_ok = regex.fullmatch(with_dot)
-
-            if dot_ok and not hash_ok:
-                row = with_dot
-            if hash_ok and not dot_ok:
-                row = with_hash
-
-    # Second pass: recurse into each valid solution
-    indexes = [i for i, x in enumerate(row) if x == '?']
-    unknown = len(indexes)
-    missing = total - row.count('#')
-    if unknown == missing or missing == 0:
-        return 1
+    if unknown == 0:
+        return [row]
+    if missing == unknown:
+        return [row.replace('?', '#')]
 
     i = indexes[0]
-    result = 0
+    result = []
     for ch in ('#', '.'):
         attempt = replace(row, i, ch)
         valid = regex.fullmatch(attempt)
         if valid:
-            result += count_ways(attempt, runs, regex, False)
+            result.extend(get_ways(attempt, runs, regex))
     return result
 
 
@@ -115,18 +62,44 @@ if __name__ == '__main__':
 
     # Part 1
     total = 0
+    part1 = []
     for row, runs in rows:
-        ways = count_ways(row, runs)
-        total += ways
+        regex = build_regex(runs)
+        simple = simplify(row, regex)
+        nways = len(get_ways(simple, runs, regex))
+        total += nways
+        part1.append(nways)
     print(total)
 
-    # Part 2 - expand all inputs rows by 5x
-    for i, [row, runs] in enumerate(rows):
-        rows[i][0] = '?'.join([row] * 5)
-        rows[i][1] = runs * 5
-
+    # Part 2
+    #
+    # We already know all the ways the expanded row can be solved WITHOUT using
+    # the new '?' delimiters - we just need to check all the ways it can be
+    # solved where we do use at least one of them.
     total = 0
-    for row, runs in rows:
-        ways = count_ways(row, runs)
-        total += ways
+    for i, nways in enumerate(part1):
+        origrow, runs = rows[i]
+
+        joins = []
+        for n in range(2, 6):
+            row = '#'.join([origrow] * n)
+            subruns = runs * n
+            regex = build_regex(subruns)
+            if not regex.fullmatch(row):
+                joins.append(0)
+                continue
+            simple = simplify(row, regex)
+            joins.append(len(get_ways(simple, subruns, regex)))
+
+        subtotal = sum([
+                nways ** 5,
+                4 * joins[0] * (nways ** 3),
+                3 * joins[1] * (nways ** 2),
+                2 * joins[2] * nways,
+                3 * (joins[0] ** 2) * nways,
+                2 * joins[0] * joins[1],
+                joins[3],
+                ])
+        print(f"R{i + 1} = {subtotal}")
+        total += subtotal
     print(total)
