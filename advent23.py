f777562b58b4c29a514d7c8276343ad13830553a
Brendan Jurd <direvus@gmail.com>
2024-09-01 09:15:48 +1000

    Solve Day 23 Part 2.
    I'm not delighted about the performance of this, it takes around 56
    seconds. I could possibly speed it up by doing a Dijkstra or a
    topological ordering.

--- advent23.py
+++ advent23.py
@@ -16,6 +16,8 @@ class Grid:
         self.start = None
         self.end = None
         self.head = None
+        self.segments = {}
+        self.slopes = True
 
     def parse(self, stream):
         for line in stream:
@@ -26,6 +28,7 @@ class Grid:
             self.rows.append(line)
         self.height = len(self.rows)
         self.end = Point(self.height - 1, line.index('.'))
+        self.segments = {}
         self.head = self.find_paths(self.start, set())
 
     def get_tile(self, point) -> str:
@@ -38,80 +41,127 @@ class Grid:
         tile = self.get_tile(point)
         return tile is not None and tile != '#'
 
-    def get_neighbours(self, point, visited) -> set:
+    def get_neighbours(self, point) -> set:
         """Return all neighbours of the tile that are valid next steps.
 
-        A valid next step must be a path tile that we have not previously
-        visited. If the current tile is a slope, the only possible valid
-        neighbour is the tile it points to.
+        If the current tile is a slope, and the grid is in slopes mode, the
+        only possible valid neighbour is the tile it points to.
         """
         tile = self.get_tile(point)
-        match tile:
-            case '>':
-                n = {Point(point.y, point.x + 1)}
-            case '<':
-                n = {Point(point.y, point.x - 1)}
-            case '^':
-                n = {Point(point.y - 1, point.x)}
-            case 'v':
-                n = {Point(point.y + 1, point.x)}
-            case _:
-                n = {
-                    Point(point.y, point.x + 1),
-                    Point(point.y, point.x - 1),
-                    Point(point.y - 1, point.x),
-                    Point(point.y + 1, point.x),
-                    }
+        if self.slopes and tile in '><^v':
+            match tile:
+                case '>':
+                    n = {Point(point.y, point.x + 1)}
+                case '<':
+                    n = {Point(point.y, point.x - 1)}
+                case '^':
+                    n = {Point(point.y - 1, point.x)}
+                case 'v':
+                    n = {Point(point.y + 1, point.x)}
+        elif point == self.start:
+            n = {Point(point.y + 1, point.x)}
+        else:
+            n = {
+                Point(point.y, point.x + 1),
+                Point(point.y, point.x - 1),
+                Point(point.y - 1, point.x),
+                Point(point.y + 1, point.x),
+                }
         # exclude walls and out-of-bounds
         n = {x for x in n if self.is_tile_valid(x)}
-        # exclude already visited
-        n -= visited
         return n
 
-    def find_paths(self, start, visited) -> Segment | None:
+    def reset_paths(self):
+        self.segments = {}
+        self.head = None
+
+    def find_paths(self, start, prev=None) -> Point | None:
         """Build a tree of valid paths through the maze."""
-        # Take a copy of the visited set and add the start node
-        visited = {start} | visited
+        if start in self.segments:
+            # We've already explored this start point, don't repeat
+            return start
         # Special case: the starting tile of the map doesn't count as a step
         length = 1 if start != self.start else 0
         end = start
         while True:
-            neighbours = self.get_neighbours(end, visited)
+            neighbours = self.get_neighbours(end)
+            if prev:
+                neighbours.discard(prev)
             if not neighbours:
                 # No valid way forward -> this is a dead end.
                 return None
             if len(neighbours) == 1:
                 # Only one way forward -> continue this segment.
                 length += 1
+                prev = end
                 (end,) = neighbours
-                visited.add(end)
                 if end == self.end:
                     # We have reached the goal.
-                    return Segment(start, end, length, [])
+                    segment = Segment(start, end, length, [])
+                    self.segments[start] = segment
+                    return start
                 continue
             # Multiple ways forward -> recurse into each choice.
             choices = []
+            segment = Segment(start, end, length, choices)
+            self.segments[start] = segment
             for n in neighbours:
-                choice = self.find_paths(n, visited)
+                choice = self.find_paths(n, end)
                 if choice is not None:
                     choices.append(choice)
-            return Segment(start, end, length, choices)
+            if not choices:
+                return None
+            return start
 
-    def get_longest_path(self, segment) -> int:
-        length = segment.length
+    def get_longest_path(
+            self, start, visited) -> tuple[int, list[Point]] | None:
+        segment = self.segments[start]
+        if start in visited or segment.end in visited:
+            return None
         if segment.end == self.end:
-            return length
-        choice_lengths = [self.get_longest_path(x) for x in segment.choices]
-        return length + max(choice_lengths)
+            return segment.length, [segment.end]
+        visited = visited | {start, segment.end}
+        choices = [
+                (self.get_longest_path(x, visited), x)
+                for x in segment.choices
+                if x not in visited]
+        choices = list(filter(lambda x: x[0] is not None, choices))
+        if not choices:
+            return None
+        choices.sort(key=lambda x: x[0][0], reverse=True)
+        (length, path), choice = choices[0]
+        path.insert(0, choice)
+        return segment.length + length, path
+
+    def get_longest_path_length(self, start, visited=None) -> int | None:
+        if visited is None:
+            visited = set()
+        segment = self.segments[start]
+        if start in visited or segment.end in visited:
+            return None
+        if segment.end == self.end:
+            return segment.length
+        visited = visited | {start, segment.end}
+        choices = [
+                self.get_longest_path_length(x, visited)
+                for x in segment.choices
+                if x not in visited]
+        choices = list(filter(lambda x: x is not None, choices))
+        if not choices:
+            return None
+        return segment.length + max(choices)
 
 
 if __name__ == '__main__':
     with timing("Part 1\n"):
         grid = Grid()
         grid.parse(sys.stdin)
-        result = grid.get_longest_path(grid.head)
+        result = grid.get_longest_path_length(grid.head)
     print(f"Result for Part 1 = {result} \n")
 
     with timing("Part 2\n"):
-        result = None
+        grid.reset_paths()
+        grid.slopes = False
+        grid.head = grid.find_paths(grid.start)
+        result = grid.get_longest_path_length(grid.head)
     print(f"Result for Part 2 = {result} \n")
