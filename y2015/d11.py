c248fc30480bbe1607c1fd3899a3feb27e59db47
Brendan Jurd <direvus@gmail.com>
2024-09-08 14:58:20 +1000

    Optimise in 2015 Day 11.
    I tried a few different things, but in the end none of it was massively
    faster than the naive implementation for the test case 'ghijklmn'.
    
    I settled on the bytearray version because it's tight and memory
    efficient, even though it's not very much faster overall. And to avoid
    bogging down the test runs, I replaced the test case with the much
    faster option 'abcdefgh'.

--- y2015/d11.py
+++ y2015/d11.py
@@ -1,54 +1,45 @@
-import string
+FORBIDDEN = {105, 108, 111}
+LENGTH = 8
 
 
-CHARS = string.ascii_lowercase
-FORBIDDEN = {'i', 'o', 'l'}
-STRAIGHTS = {
-        'abc', 'bcd', 'cde', 'def', 'efg', 'fgh', 'pqr', 'qrs',
-        'rst', 'stu', 'tuv', 'uvw', 'vwx', 'wxy', 'xyz'}
-PAIRS = {
-        'aa', 'bb', 'cc', 'dd', 'ee', 'ff', 'gg', 'hh',
-        'jj', 'kk', 'mm', 'nn', 'pp', 'qq', 'rr', 'ss',
-        'tt', 'uu', 'vv', 'ww', 'xx', 'yy', 'zz'}
+def is_valid(value: bytearray) -> bool:
+    doubles = set()
+    triples = False
+    for i, b in enumerate(value):
+        if b in FORBIDDEN:
+            return False
+        if i < LENGTH - 2 and value[i + 1] == b + 1 and value[i + 2] == b + 2:
+            triples = True
+        if i < LENGTH - 1 and value[i + 1] == b and b not in doubles:
+            doubles.add(b)
+    return triples and len(doubles) >= 2
 
 
-def is_valid(value: str) -> bool:
-    if FORBIDDEN & set(value):
-        return False
-    triples = {value[i - 3:i] for i in range(3, len(value) + 1)}
-    if not triples & STRAIGHTS:
-        return False
-    doubles = {value[i - 2:i] for i in range(2, len(value) + 1)}
-    return len(doubles & PAIRS) >= 2
-
-
-def gen_next_string(value: str) -> str:
-    chars = list(value)
-    i = len(chars) - 1
+def increment_string(value: bytearray) -> None:
+    i = len(value) - 1
     while i >= 0:
-        ch = chars[i]
-        if ch == CHARS[-1]:
-            chars[i] = CHARS[0]
+        ch = value[i]
+        if ch == 122:
+            value[i] = 97
         else:
-            index = CHARS.index(ch)
-            chars[i] = CHARS[index + 1]
-            break
+            value[i] += 1
+            return
         i -= 1
-    return ''.join(chars)
 
 
-def gen_next_valid_string(value: str) -> str:
+def gen_next_valid_string(value: bytearray) -> bytearray:
     valid = False
     while not valid:
-        value = gen_next_string(value)
+        increment_string(value)
         valid = is_valid(value)
     return value
 
 
 def run(stream, test=False):
-    value = stream.readline().strip()
+    value = bytearray(stream.readline().strip(), 'ascii')
 
-    result1 = gen_next_valid_string(value)
-    result2 = gen_next_valid_string(result1)
+    v1 = gen_next_valid_string(value)
+    result1 = v1.decode('ascii')
+    result2 = gen_next_valid_string(v1).decode('ascii')
 
     return (result1, result2)
