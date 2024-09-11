85e7bf288fe1dd0bc7c9279cd54afce86d4fc90b
Brendan Jurd <direvus@gmail.com>
2024-09-11 23:06:29 +1000

    Fail again to solve 2015 Day 19 Part 2.
    Following a lead about context-free grammars from this post:
    
    https://www.reddit.com/r/adventofcode/comments/16calg6/was_the_real_solution_to_2015_day_19_ever_revealed/
    
    I went down the rabbit hole of writing a CYK algorithm, which required
    me to also write a transforming function to put the production rules
    from the puzzle into Chomsky Normal Form.
    
    Boy did that turn out to be a huge waste of effort, because CYK
    apparently only works on target strings that consist entirely of
    terminal symbols. Which the puzzle molecule very definitely is not.
    
    This code works very well on the example grammar from the Wikipedia page
    on CYK but it doesn't work for the puzzle input, or even for the HOHOHO
    test case.
    
    I'm committing this in case I ever need to refer back to my solution,
    for a situation that actually *does* call for CYK or Chomsky Normal
    Form.

--- y2015/d19.py
+++ y2015/d19.py
@@ -1,125 +1,14 @@
-import heapq
-import math
 import re
 from collections import defaultdict
+from copy import copy
 
 
 INF = float('inf')
 ATOM = re.compile(r'[A-Z][a-z]?')
 
 
-class PriorityQueue:
-    def __init__(self):
-        self.queue = []
-        self.finder = {}
-        self.deleted = set()
-
-    def __len__(self):
-        return len(self.queue)
-
-    def __bool__(self):
-        return bool(self.finder)
-
-    def push(self, node, priority):
-        entry = (priority, node)
-        heapq.heappush(self.queue, entry)
-        self.finder[node] = entry
-
-    def has_node(self, node):
-        return node in self.finder
-
-    def has_position(self, position):
-        return position in {(n.y, n.x) for n in self.finder.keys()}
-
-    def set_priority(self, node, priority):
-        if node in self.finder:
-            self.deleted.add(id(self.finder[node]))
-        self.push(node, priority)
-
-    def pop(self):
-        while self.queue:
-            entry = heapq.heappop(self.queue)
-            if id(entry) not in self.deleted:
-                del self.finder[entry[1]]
-                return entry
-            self.deleted.discard(id(entry))
-        raise KeyError('Cannot pop from empty priority queue')
-
-
 def get_atoms(molecule: str) -> tuple[str]:
     return tuple(ATOM.findall(molecule))
-
-
-def get_common_prefix_len(a, b) -> int:
-    length = min(len(a), len(b))
-    for i in range(length):
-        if a[i] != b[i]:
-            return i
-    return length
-
-
-def get_neighbours(
-        molecule: tuple, reps: set,
-        maxdiff: int = INF, prefix: int = 0) -> set[str]:
-    outputs = set()
-    for src, dst in reps:
-        index = prefix
-        diff = len(dst) - len(src)
-        if diff > maxdiff:
-            continue
-        while src in molecule[index:]:
-            index = molecule.index(src, index)
-            output = molecule[:index] + dst + molecule[index + 1:]
-            outputs.add(output)
-            index += 1
-    return outputs
-
-
-def get_min_distance(start: tuple, dest: tuple, maxdiff: int) -> float:
-    """Return the best-case distance between two strings."""
-    diff = len(dest) - len(start)
-    if diff == 0:
-        return 1
-    if diff < 0:
-        # Replacements can't ever make the string shorter
-        return INF
-    return math.ceil(diff / maxdiff)
-
-
-def find_path_astar(dest: tuple, reps: set, start: tuple = ('e',)) -> int:
-    nodes = PriorityQueue()
-    length = len(dest)
-    maxdiff = max(len(b) for a, b in reps) - 1
-    nodes.push(start, 1)
-    origins = {}
-    g = defaultdict(lambda: INF)
-    g[start] = 0
-
-    while nodes:
-        cost, current = nodes.pop()
-        if current == dest:
-            n = current
-            path = [n]
-            while n in origins:
-                n = origins[n]
-                path.insert(0, n)
-            print(path)
-            return g[current]
-
-        diff = length - len(current)
-        prefix = get_common_prefix_len(current[:-1], dest)
-        neighbours = get_neighbours(current, reps, diff, prefix)
-        for neighbour in neighbours:
-            score = g[current] + 1
-            if score < g[neighbour]:
-                # Best path to the neighbour so far
-                origins[neighbour] = current
-                g[neighbour] = score
-
-                dist = len(current)
-                fscore = score + dist
-                nodes.set_priority(neighbour, fscore)
-    print("Ran out of nodes without finding the destination!")
 
 
 def parse(stream) -> tuple[set, tuple]:
@@ -134,13 +23,125 @@ def parse(stream) -> tuple[set, tuple]:
     return reps, target
 
 
+def replace_atoms(molecule: tuple, atom: str, replacement: str) -> tuple:
+    """Return a new molecule tuple by replacing all instances of an atom."""
+    result = molecule
+    index = 0
+    while atom in result[index:]:
+        index = result.index(atom, index)
+        result = result[:index] + (replacement,) + result[index + 1:]
+    return result
+
+
+def normalise_chomsky(reps: set) -> set:
+    terminals = set()
+    nonterminals = set()
+    for a, b in reps:
+        nonterminals.add(a)
+        terminals |= set(b)
+    terminals -= nonterminals
+
+    # Skipping the START step because we know that the start symbol doesn't
+    # appear on the right-hand side in the grammar.
+    n = 1
+    new = {}
+    for t in terminals:
+        new[t] = f'N{n}'
+        n += 1
+
+    # TERM: Eliminate rules that have non-solitary terminals
+    result = copy(reps)
+    for a, b in reps:
+        terms = set(b) & terminals
+        if terms and len(b) > 1:
+            result.discard((a, b))
+            for t in terms:
+                result.add((new[t], (t,)))
+                b = replace_atoms(b, t, new[t])
+            result.add((a, b))
+    # BIN: Now find rules that have more than 2 outputs, and just so I'm not
+    # looping over the thing that I'm modifying, copy those into a separate
+    # variable first.
+    longs = [(a, b) for a, b in result if len(b) > 2]
+    n = 1
+    for a, b in longs:
+        while len(b) > 2:
+            # Rule has more than 2 outputs
+            t = f'A{n}'
+            n += 1
+            result.discard((a, b))
+            result.add((a, (b[0], t)))
+            a = t
+            b = b[1:]
+            result.add((a, b))
+
+    # DEL: Skipping this step because there are no empty rules
+    empty = {a for a, b in result if len(b) == 0}
+    assert not empty
+
+    # UNIT: Eliminate unit rules
+    nonterminals = {a for a, _ in result}
+    units = [(a, b) for a, b in result if len(b) == 1 and b[0] in nonterminals]
+    new = set()
+    for a, b in units:
+        result.discard((a, b))
+        for r in result:
+            if r[0] == b[0] and r not in units:
+                new.add((a, r[1]))
+    return result | new
+
+
+def get_parse_trees(molecule: tuple, reps: set, start: str = 'e'):
+    n = len(molecule)
+    r = normalise_chomsky(reps)
+    routes = defaultdict(lambda: False)  # (length, start, source): bool
+    back = defaultdict(list)
+
+    for i in range(n):
+        for a, b in r:
+            if b == molecule[i:i + 1]:
+                routes[(1, i, a)] = True
+
+    print(routes)
+    for l in range(2, n + 1):
+        for s in range(n - l + 1):
+            for p in range(1, l):
+                print(f'Considering a {l} long substring from {s}, partitioned at {p}: {molecule[s:s+l]}')
+                for a, b in r:
+                    if len(b) != 2:
+                        continue
+                    b, c = b
+                    if routes[(p, s, b)] and routes[(l - p, s + p, c)]:
+                        print(f"  Can be produced by {a} => {b},{c}")
+                        routes[(l, s, a)] = True
+                        back[(l, s, a)].append((p, b, c))
+    print(routes)
+    print(back)
+
+
+def get_neighbours(molecule: tuple, reps: set) -> set[str]:
+    outputs = set()
+    for src, dst in reps:
+        index = 0
+        while src in molecule[index:]:
+            index = molecule.index(src, index)
+            output = molecule[:index] + dst + molecule[index + 1:]
+            outputs.add(output)
+            index += 1
+    return outputs
+
+
 def count_outputs(reps: set, molecule: tuple) -> int:
     outputs = get_neighbours(molecule, reps)
     return len(outputs)
 
 
+def count_steps(target: tuple, reps: set, start: str = 'e') -> int:
+    result = get_parse_trees(target, reps, start)
+
+
 def run(stream, test=False, draw=False):
     reps, molecule = parse(stream)
     result1 = count_outputs(reps, molecule)
-    result2 = find_path_astar(molecule, reps)
+    result2 = count_steps(molecule, reps)
     return (result1, result2)
