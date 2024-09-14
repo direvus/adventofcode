9aa4e45231263df9e3f87f9856bb69d780c8a136
Brendan Jurd <direvus@gmail.com>
2024-09-15 08:54:44 +1000

    Solve 2016 Day 7 Part 2.

--- y2016/d07.py
+++ y2016/d07.py
@@ -25,18 +25,43 @@ def supports_tls(ip: str) -> bool:
     return abba
 
 
-def count_tls(ips: list) -> int:
-    count = 0
+def supports_ssl(ip: str) -> bool:
+    abas = set()
+    babs = set()
+    brackets = False
+    for i, ch in enumerate(ip):
+        if ch == '[':
+            brackets = True
+        elif ch == ']':
+            brackets = False
+        elif i >= 2:
+            if (ip[i] == ip[i - 2] and ip[i - 1] != ip[i]):
+                chunk = ip[i - 2: i + 1]
+                reverse = chunk[1] + chunk[0] + chunk[1]
+                if brackets:
+                    babs.add(chunk)
+                    if reverse in abas:
+                        return True
+                else:
+                    abas.add(chunk)
+                    if reverse in babs:
+                        return True
+    return False
+
+
+def count_tls_ssl(ips: list) -> tuple:
+    tls = 0
+    ssl = 0
     for ip in ips:
         if supports_tls(ip):
-            count += 1
-    return count
+            tls += 1
+        if supports_ssl(ip):
+            ssl += 1
+    return tls, ssl
 
 
 def run(stream, test=False, draw=False):
     packets = parse(stream)
 
-    result1 = count_tls(packets)
-    result2 = 0
-
+    result1, result2 = count_tls_ssl(packets)
     return (result1, result2)
