95e7b62a1b49712e9ef69d4d14f87fcf58228706
Brendan Jurd <direvus@gmail.com>
2024-09-16 11:13:50 +1000

    Add a visualiser for 2016 Day 13 Part 1.

--- y2016/d13.py
+++ y2016/d13.py
@@ -1,4 +1,16 @@
 from functools import cache
+try:
+    from PIL import Image
+except ImportError:
+    # That's fine, visualisations won't be available though.
+    pass
+
+
+PIXELS = {
+        'path': 'assets/green_pixel_4.png',
+        'explored': 'assets/orange_pixel_4.png',
+        'wall': 'assets/grey_pixel_4.png',
+        }
 
 
 @cache
@@ -39,7 +51,8 @@ def trace_to_path(trace: dict, end: tuple) -> list:
     return result
 
 
-def find_fewest_moves(start: tuple, goal: tuple, magic: int) -> int:
+def find_fewest_moves(
+        start: tuple, goal: tuple, magic: int, draw: bool = False) -> int:
     """Find the fewest number of moves needed to reach the goal.
 
     The `magic` number is applied to `is_space()` to discover which locations
@@ -48,12 +61,28 @@ def find_fewest_moves(start: tuple, goal: tuple, magic: int) -> int:
     explored = {start}
     trace = {}
     q = [(0, start)]
+    frames = []
 
     while q:
         cost, node = q.pop(0)
         if node == goal:
+            if draw:
+                path = set(trace_to_path(trace, node))
+                frames.append((explored, path))
+
+                maxcoord = max(max(p) for p in explored) + 2
+                size = 5 * maxcoord + 1  # 4 pixels per cell, plus border
+
+                bg = draw_background(magic, size)
+
+                images = [draw_frame(bg, *f) for f in frames]
+                images[0].save(
+                        'out/y2016d13p1_bfs.gif', save_all=True,
+                        append_images=images[1:], duration=100)
             return cost
 
+        if draw:
+            frames.append((set(explored), {start, goal}))
         for n in get_neighbours(node, magic):
             if n in explored:
                 continue
@@ -63,11 +92,38 @@ def find_fewest_moves(start: tuple, goal: tuple, magic: int) -> int:
     raise ValueError("Ran out of moves to try!")
 
 
+def draw_background(magic: int, size: int):
+    im = Image.new('RGB', (size, size), '#1a1a1a')
+    wall = Image.open(PIXELS['wall'])
+    for i in range(size):
+        for j in range(size):
+            y = 1 + i * 5
+            x = 1 + j * 5
+            if not is_space(j, i, magic):
+                im.paste(wall, (x, y))
+    return im
+
+
+def draw_frame(bg, explored: set, path: set):
+    im = bg.copy()
+    pixel = Image.open(PIXELS['explored'])
+    for p in explored:
+        x = 1 + p[0] * 5
+        y = 1 + p[1] * 5
+        im.paste(pixel, (x, y))
+    pixel = Image.open(PIXELS['path'])
+    for p in path:
+        x = 1 + p[0] * 5
+        y = 1 + p[1] * 5
+        im.paste(pixel, (x, y))
+    return im
+
+
 def run(stream, test=False, draw=False):
     magic = int(stream.readline().strip())
     goal = (7, 4) if test else (31, 39)
 
-    result1 = find_fewest_moves((1, 1), goal, magic)
+    result1 = find_fewest_moves((1, 1), goal, magic, draw)
     result2 = 0
 
     return (result1, result2)
