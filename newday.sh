e79def8bb38acfe9a6e804a15b959dd028b522e5
Brendan Jurd <direvus@gmail.com>
2024-09-26 22:38:57 +1000

    Improve the newday script.
    It now avoids fiddling with the test suite if it already has a test case
    for the given day.

--- newday.sh
+++ newday.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
-AOC_YEAR="$1"
-AOC_DAY="$2"
-
-export AOC_YEAR
-export AOC_DAY
-
+#
+# newday.sh YEAR DAY
+#
+# Set up a new puzzle day for Advent of Code.
+#
+# This script is meant to be *sourced* (not executed), and it will set the
+# AOC_YEAR, AOC_DAY and AOC_DD environment variables, create a new test input
+# file, add a test case to the test suite for the year, copy a new Python file
+# from the skeleton for the day, and schedule files to be added to git.
+yyyy="$1"
+d="$2"
 dd="$(printf %02d $AOC_DAY)"
 
-touch "y$AOC_YEAR/tests/$dd"
-sed "s/_YEAR_/$AOC_YEAR/; s/_DAY_/$AOC_DAY/" skeleton.py > "y$AOC_YEAR/d$dd.py"
-echo -e "
+export AOC_YEAR="$yyyy"
+export AOC_DAY="$d"
+export AOC_DD="$dd"
 
-def test_y${AOC_YEAR}d${dd}():
-    assert get_day_result($AOC_DAY) == (0, 0)" >> "tests/test_${AOC_YEAR}.py"
+touch "y$yyyy/tests/$dd"
+sed "s/_YEAR_/$yyyy/; s/_DAY_/$d/" skeleton.py > "y$yyyy/d$dd.py"
 
-git add -N "y$AOC_YEAR/tests/$dd" "y$AOC_YEAR/d$dd.py"
+if ! fgrep -q "test_y${yyyy}d${dd}" "tests/test_$yyyy.py"; then
+    echo -e "
+
+def test_y${yyyy}d${dd}():
+    assert get_day_result($d) == (0, 0)" >> "tests/test_${yyyy}.py"
+fi
+
+git add -N "y$yyyy/tests/$dd" "y$yyyy/d$dd.py"
