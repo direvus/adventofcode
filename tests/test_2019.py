34a0e9c5871d0edbafc04389a4b2a54f771e8067
Brendan Jurd <direvus@gmail.com>
2024-10-07 10:09:51 +1100

    Set up for 2019.

---
+++ tests/test_2019.py
@@ -0,0 +1,14 @@
+import importlib
+import os
+
+
+YEAR = 2019
+
+
+def get_day_result(day):
+    modpath = f'y{YEAR}.d{day:02d}'
+    inpath = os.path.join(f'y{YEAR}', 'tests', f'{day:02d}')
+    m = importlib.import_module(modpath)
+    with open(inpath, 'r') as infile:
+        result = m.run(infile, test=True)
+    return result
