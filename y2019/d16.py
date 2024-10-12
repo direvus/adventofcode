5341a8293114f888996b43015f75698561f7cd78
Brendan Jurd <direvus@gmail.com>
2024-10-12 14:27:57 +1100

    Further optimise 2019 Day 16.
    Using JIT precompiled code gets us about 6x improvement, so the full
    test case takes about 11s. That's much better, but still not good enough
    to run the actual input.

--- y2019/d16.py
+++ y2019/d16.py
@@ -7,7 +7,7 @@ https://adventofcode.com/2019/day/16
 import logging  # noqa: F401
 from itertools import cycle
 
-from util import timing
+from util import jit, timing
 
 
 BASE = (0, 1, 0, -1)
@@ -19,6 +19,7 @@ def expand_pattern(pattern, repeats: int):
             yield item
 
 
+@jit
 def get_slices(size: int, index: int) -> tuple:
     """Return the positive and negative slices of the pattern."""
     pos = []
@@ -33,6 +34,7 @@ def get_slices(size: int, index: int) -> tuple:
     return pos, neg
 
 
+@jit
 def get_element(inputs, index: int) -> int:
     result = 0
     pos, neg = get_slices(len(inputs), index)
@@ -43,6 +45,7 @@ def get_element(inputs, index: int) -> int:
     return abs(result) % 10
 
 
+@jit
 def do_phase(signal: list, offset: int = 0) -> None:
     """Perform one phase of transforms on the signal.
 
@@ -52,16 +55,17 @@ def do_phase(signal: list, offset: int = 0) -> None:
         signal[i] = get_element(signal, i)
 
 
-def do_phases(inputs, count: int, offset: int = 0, length: int = 8) -> tuple:
+@jit
+def do_phases(inputs, count: int, offset: int = 0, length: int = 8) -> list:
     signal = list(inputs)
     for _ in range(count):
         do_phase(signal, offset)
-    return tuple(signal[offset:offset + length])
+    return signal[offset:offset + length]
 
 
-def parse(stream) -> tuple[int]:
+def parse(stream) -> list[int]:
     line = stream.readline().strip()
-    return tuple(int(x) for x in line)
+    return [int(x) for x in line]
 
 
 def run(stream, test: bool = False):
@@ -72,7 +76,7 @@ def run(stream, test: bool = False):
 
     with timing("Part 2"):
         if test:
-            signal = tuple(map(int, '03036732577212944063491565474664'))
+            signal = list(map(int, '03036732577212944063491565474664'))
         offset = int(''.join(str(x) for x in signal[:7]))
         signal = signal * 10_000
         output = do_phases(signal, 100, offset, 8)
