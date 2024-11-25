29b053ca5fa7f9e1961360441272e4453dfeff4c
Brendan Jurd <direvus@gmail.com>
2024-11-25 20:36:32 +1100

    Optimise 2022 Day 16 part 1.
    Got much better performance (<1s) by pre-computing the optimal path from
    each node to each of the openable valves, and then we can effectively
    ignore all the nodes that don't have zero flow.
    
    I'm still pondering on how to do part 2 effectively. Having two agents
    really puts a spanner in the works.

--- y2022/d16.py
+++ y2022/d16.py
@@ -7,8 +7,9 @@ https://adventofcode.com/2022/day/16
 import logging  # noqa: F401
 import re
 from collections import defaultdict
+from itertools import combinations
 
-from util import timing, NINF, PriorityQueue
+from util import timing, INF, NINF, PriorityQueue
 
 
 PATTERN = re.compile(
@@ -19,6 +20,7 @@ class Graph:
     def __init__(self):
         self.nodes = {}
         self.edges = defaultdict(set)
+        self.paths = defaultdict(list)
 
     def parse(self, stream):
         for line in stream:
@@ -27,12 +29,68 @@ class Graph:
             name, flow, tunnels = m.groups()
             self.nodes[name] = int(flow)
             self.edges[name] = set(tunnels.split(', '))
+        self.find_paths()
+
+    def find_path(self, start: str, end: str):
+        """Find the best route between two nodes.
+
+        Return the best path as a list of node names, including the start and
+        end nodes.
+        """
+        if end in self.edges[start]:
+            # The nodes are adjacent
+            return [start, end]
+
+        # Dijkstra
+        q = PriorityQueue()
+        q.push(start, 0)
+        dist = defaultdict(lambda: INF)
+        dist[start] = 0
+        explored = set()
+        sources = {}
+
+        while q:
+            cost, node = q.pop()
+
+            if node == end:
+                break
+
+            score = cost + 1
+            for n in self.edges[node]:
+                if n in explored:
+                    continue
+                if score < dist[n]:
+                    dist[n] = score
+                    sources[n] = node
+                    q.set_priority(n, score)
+            explored.add(node)
+        result = [end]
+        while node in sources:
+            node = sources[node]
+            result.append(node)
+        result.reverse()
+        return result
+
+    def find_paths(self):
+        """Find the best route between each pair of openable valves."""
+        valves = {n for n in self.nodes if n == 'AA' or self.nodes[n] > 0}
+        for a, b in combinations(valves, 2):
+            path = self.find_path(a, b)
+            for i in range(len(path) - 1):
+                self.paths[frozenset({path[i], b})] = path[i:]
+
+    def get_next_node_on_path(self, start, end):
+        path = self.paths[frozenset({start, end})]
+        if path[0] == start:
+            return path[1]
+        else:
+            return path[-2]
 
     def find_best_release(self, limit: int = 30):
         """Find the best amount of pressure that can be released.
 
         Given `limit` units of time, find the path through the graph (possibly
-        opening the valve at each node) that will ultimately release the most
+        opening a valve at each node) that will ultimately release the most
         pressure.
 
         Once a valve is open, it releases pressure equal to its flow rate every
@@ -47,7 +105,8 @@ class Graph:
         released = defaultdict(lambda: NINF)
         released[start] = 0
         best = 0
-        size = len(self.nodes)
+        targets = {node for node, flow in self.nodes.items() if flow > 0}
+        size = len(targets)
 
         while q:
             priority, node = q.pop()
@@ -57,28 +116,21 @@ class Graph:
             if pressure > best:
                 best = pressure
 
-            if time == limit or len(opened) == size:
-                # If we've run out of time, or all the valves are opened, then
-                # there are no further moves to be made.
+            if time >= limit or len(opened) == size:
+                # If we've run out of time, or all the valves that can be
+                # opened are open, then there are no further moves to be made.
                 continue
 
-            flow = self.nodes[valve]
-            newtime = time + 1
-            if flow > 0 and valve not in opened:
-                # We could open the valve
-                newopen = frozenset(opened | {valve})
-                release = flow * (limit - (time + 1)) + pressure
-                new = (valve, newtime, newopen)
-                if release > released[new]:
+            # For each remaining openable valve, queue up a move to that valve.
+            for target in targets - opened - {valve}:
+                path = self.paths[frozenset({valve, target})]
+                newtime = time + len(path)
+                new = (target, newtime, frozenset(opened | {target}))
+                flow = self.nodes[target]
+                release = flow * (limit - newtime) + pressure
+                if newtime <= limit and release > released[new]:
                     released[new] = release
                     q.set_priority(new, (-release, newtime))
-
-            for edge in self.edges[valve]:
-                # We could move to another valve
-                new = (edge, newtime, opened)
-                if pressure > released[new]:
-                    released[new] = pressure
-                q.set_priority(new, (-pressure, newtime))
         return best
 
 
