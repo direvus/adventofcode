133d471a740e5c349a77c318f7bffe83822cc5dc
Brendan Jurd <direvus@gmail.com>
2024-12-04 18:42:32 +1100

    Solve 2024 Day 4, part 2.

--- tests/test_2024.py
+++ tests/test_2024.py
@@ -17,4 +17,4 @@ def test_y2024d03():
 
 
 def test_y2024d04():
-    assert get_day_result(YEAR, 4) == (18, 0)
+    assert get_day_result(YEAR, 4) == (18, 9)
--- y2024/d04.py
+++ y2024/d04.py
@@ -52,12 +52,34 @@ def count_word(lines, word) -> int:
     return result
 
 
+def count_x_word_at_position(lines, size, targets, row, col) -> int:
+    result = 0
+    a = ''.join(lines[row + i][col + i] for i in range(size))
+    b = ''.join(lines[row + i][col + size - 1 - i] for i in range(size))
+
+    if a in targets and b in targets:
+        result += 1
+    return result
+
+
+def count_x_word(lines, word) -> int:
+    result = 0
+    size = len(word)
+    rev = ''.join(reversed(word))
+    targets = {word, rev}
+    for row in range(len(lines) - size + 1):
+        line = lines[row]
+        for col in range(len(line) - size + 1):
+            result += count_x_word_at_position(lines, size, targets, row, col)
+    return result
+
+
 def run(stream, test: bool = False):
     with timing("Part 1"):
         lines = parse(stream)
         result1 = count_word(lines, 'XMAS')
 
     with timing("Part 2"):
-        result2 = 0
+        result2 = count_x_word(lines, 'MAS')
 
     return (result1, result2)
