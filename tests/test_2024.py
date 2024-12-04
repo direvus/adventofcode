aa6221eb6fac52ae6c0dac18616fc3a26a1a324c
Brendan Jurd <direvus@gmail.com>
2024-12-04 18:35:33 +1100

    Solve 2024 Day 4, part 1.

--- tests/test_2024.py
+++ tests/test_2024.py
@@ -14,3 +14,7 @@ def test_y2024d02():
 
 def test_y2024d03():
     assert get_day_result(YEAR, 3) == (161, 48)
+
+
+def test_y2024d04():
+    assert get_day_result(YEAR, 4) == (18, 0)
---
+++ y2024/d04.py
@@ -0,0 +1,63 @@
+"""Advent of Code 2024
+
+Day 4: _TITLE_
+
+https://adventofcode.com/2024/day/4
+"""
+import logging  # noqa: F401
+
+from util import timing
+
+
+def parse(stream) -> str:
+    return tuple(x.strip() for x in stream)
+
+
+def count_word_at_position(lines, size, targets, row, col) -> int:
+    result = 0
+    line = lines[row]
+    width = len(line)
+    snip = line[col: col + size]
+    if snip in targets:
+        logging.debug(f'found {snip} horizontally at {row}, {col}')
+        result += 1
+    if row <= len(lines) - size:
+        vert = ''.join(line[col] for line in lines[row: row + size])
+        if vert in targets:
+            logging.debug(f'found {vert} vertically at {row}, {col}')
+            result += 1
+
+        if col <= width - size:
+            diag = ''.join(lines[row + i][col + i] for i in range(size))
+            if diag in targets:
+                logging.debug(f'found {diag} diagonally at {row}, {col}')
+                result += 1
+        if col >= size - 1:
+            diag = ''.join(lines[row + i][col - i] for i in range(size))
+            if diag in targets:
+                logging.debug(f'found {diag} diagonally at {row}, {col}')
+                result += 1
+    return result
+
+
+def count_word(lines, word) -> int:
+    result = 0
+    size = len(word)
+    rev = ''.join(reversed(word))
+    targets = {word, rev}
+    for row in range(len(lines)):
+        line = lines[row]
+        for col in range(len(line)):
+            result += count_word_at_position(lines, size, targets, row, col)
+    return result
+
+
+def run(stream, test: bool = False):
+    with timing("Part 1"):
+        lines = parse(stream)
+        result1 = count_word(lines, 'XMAS')
+
+    with timing("Part 2"):
+        result2 = 0
+
+    return (result1, result2)
