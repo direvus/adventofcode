11937fa2aadd29d08825d120d1bcc8780986a264
Brendan Jurd <direvus@gmail.com>
2025-12-01 17:28:54 +1100

    Solve 2025 Day 1, Part 2.

--- tests/test_2025.py
+++ tests/test_2025.py
@@ -5,4 +5,4 @@ YEAR = 2025
 
 
 def test_y2025d01():
-    assert get_day_result(YEAR, 1) == (3, 0)
+    assert get_day_result(YEAR, 1) == (3, 6)
--- y2025/d01.py
+++ y2025/d01.py
@@ -19,22 +19,33 @@ def parse(stream) -> list[int]:
     return result
 
 
-def count_zeroes(steps):
+def count_zeroes(moves):
     result = 0
     pos = 50
-    for step in steps:
-        pos = (pos + step) % 100
+    for move in moves:
+        pos = (pos + move) % 100
         if pos == 0:
             result += 1
     return result
 
 
+def count_zero_clicks(moves):
+    result = 0
+    pos = 50
+    for move in moves:
+        step = 1 if move > 0 else -1
+        for _ in range(abs(move)):
+            pos = (pos + step) % 100
+            if pos == 0:
+                result += 1
+    return result
+
 def run(stream, test: bool = False):
     with timing("Part 1"):
-        parsed = parse(stream)
+        parsed = tuple(parse(stream))
         result1 = count_zeroes(parsed)
 
     with timing("Part 2"):
-        result2 = 0
+        result2 = count_zero_clicks(parsed)
 
     return (result1, result2)
